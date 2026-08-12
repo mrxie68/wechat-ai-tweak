aa921c4936f8f9b64e6e98b5bb81bb5a8b6fb6ae
mrxie68 <mrxie6@gmail.com>
2026-08-12 13:00:05 +0800

    设置页整体重做：卡片式分组布局、上下留白、标签对齐、键盘自动上移、深色模式适配

--- AIPromptEditorViewController.m
+++ AIPromptEditorViewController.m
@@ -3,22 +3,61 @@
 #import "AIConfig.h"
 
 @interface AIPromptEditorViewController ()
-@property (nonatomic, strong) UITextView *textView;
-@property (nonatomic, strong) UIButton *resetButton;
-@property (nonatomic, strong) UITextField *apiKeyField;
-@property (nonatomic, strong) UITextField *modelField;
-@property (nonatomic, strong) UISwitch *enabledSwitch;
+@property (nonatomic, strong) UIView *switchCard;
 @property (nonatomic, strong) UILabel *enabledLabel;
+@property (nonatomic, strong) UISwitch *enabledSwitch;
+@property (nonatomic, strong) UIView *keyCard;
+@property (nonatomic, strong) UILabel *keyLabel;
+@property (nonatomic, strong) UITextField *apiKeyField;
+@property (nonatomic, strong) UIView *modelCard;
+@property (nonatomic, strong) UILabel *modelLabel;
+@property (nonatomic, strong) UITextField *modelField;
+@property (nonatomic, strong) UIView *delayCard;
+@property (nonatomic, strong) UILabel *delayLabel;
 @property (nonatomic, strong) UITextField *delayField;
+@property (nonatomic, strong) UIView *promptCard;
+@property (nonatomic, strong) UILabel *promptLabel;
+@property (nonatomic, strong) UITextView *textView;
+@property (nonatomic, strong) UILabel *promptHint;
+@property (nonatomic, strong) UIButton *resetButton;
+@property (nonatomic) BOOL keyboardVisible;
 @end
 
 @implementation AIPromptEditorViewController
 
+static UIView *makeCard(void) {
+    UIView *card = [[UIView alloc] initWithFrame:CGRectZero];
+    card.backgroundColor = [UIColor secondarySystemGroupedBackgroundColor];
+    card.layer.cornerRadius = 12;
+    card.layer.masksToBounds = YES;
+    return card;
+}
+
+static UILabel *makeRowLabel(NSString *text) {
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+    label.text = text;
+    label.font = [UIFont systemFontOfSize:16];
+    label.textColor = [UIColor labelColor];
+    return label;
+}
+
+static UITextField *makeRowField(NSString *placeholder) {
+    UITextField *field = [[UITextField alloc] initWithFrame:CGRectZero];
+    field.placeholder = placeholder;
+    field.borderStyle = UITextBorderStyleNone;
+    field.font = [UIFont systemFontOfSize:15];
+    field.textAlignment = NSTextAlignmentRight;
+    field.autocapitalizationType = UITextAutocapitalizationTypeNone;
+    field.autocorrectionType = UITextAutocorrectionTypeNo;
+    field.clearButtonMode = UITextFieldViewModeWhileEditing;
+    return field;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.title = @"AI 提示词";
-    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"AI 设置";
+    self.view.backgroundColor = [UIColor systemGroupedBackgroundColor];
 
     UIBarButtonItem *cancel = [[UIBarButtonItem alloc] initWithTitle:@"取消"
                                                                style:UIBarButtonItemStylePlain
@@ -32,52 +71,80 @@
                                                             action:@selector(saveTapped)];
     self.navigationItem.rightBarButtonItem = save;
 
-    self.enabledLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    self.enabledLabel.text = @"机器人开关";
-    self.enabledLabel.font = [UIFont systemFontOfSize:15];
-    [self.view addSubview:self.enabledLabel];
-
-    self.enabledSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
+    // 机器人开关
+    self.switchCard = makeCard();
+    [self.view addSubview:self.switchCard];
+    self.enabledLabel = makeRowLabel(@"机器人开关");
+    [self.switchCard addSubview:self.enabledLabel];
+    self.enabledSwitch = [[UISwitch alloc] initWithFrame:CGRectZero];
     self.enabledSwitch.on = [AISettings enabled];
-    [self.view addSubview:self.enabledSwitch];
+    [self.switchCard addSubview:self.enabledSwitch];
 
-    self.apiKeyField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    self.apiKeyField.placeholder = @"API Key（sk- 开头）";
+    // API Key
+    self.keyCard = makeCard();
+    [self.view addSubview:self.keyCard];
+    self.keyLabel = makeRowLabel(@"API Key");
+    [self.keyCard addSubview:self.keyLabel];
+    self.apiKeyField = makeRowField(@"sk- 开头");
     self.apiKeyField.text = [AISettings apiKey];
-    self.apiKeyField.borderStyle = UITextBorderStyleRoundedRect;
-    self.apiKeyField.autocapitalizationType = UITextAutocapitalizationTypeNone;
-    self.apiKeyField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.apiKeyField.font = [UIFont systemFontOfSize:14];
-    [self.view addSubview:self.apiKeyField];
+    [self.keyCard addSubview:self.apiKeyField];
 
-    self.modelField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    self.modelField.placeholder = @"模型名（如 deepseek-chat）";
+    // 模型
+    self.modelCard = makeCard();
+    [self.view addSubview:self.modelCard];
+    self.modelLabel = makeRowLabel(@"模型名");
+    [self.modelCard addSubview:self.modelLabel];
+    self.modelField = makeRowField(@"deepseek-chat");
     self.modelField.text = [AISettings model];
-    self.modelField.borderStyle = UITextBorderStyleRoundedRect;
-    self.modelField.autocapitalizationType = UITextAutocapitalizationTypeNone;
-    self.modelField.autocorrectionType = UITextAutocorrectionTypeNo;
-    self.modelField.font = [UIFont systemFontOfSize:14];
-    [self.view addSubview:self.modelField];
+    [self.modelCard addSubview:self.modelField];
 
-    self.delayField = [[UITextField alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    self.delayField.placeholder = @"思考延迟（秒）";
-    self.delayField.text = [NSString stringWithFormat:@"%.1f", [AISettings replyDelay]];
-    self.delayField.borderStyle = UITextBorderStyleRoundedRect;
+    // 思考延迟
+    self.delayCard = makeCard();
+    [self.view addSubview:self.delayCard];
+    self.delayLabel = makeRowLabel(@"思考延迟");
+    [self.delayCard addSubview:self.delayLabel];
+    self.delayField = makeRowField(@"秒");
     self.delayField.keyboardType = UIKeyboardTypeDecimalPad;
-    self.delayField.font = [UIFont systemFontOfSize:14];
-    [self.view addSubview:self.delayField];
+    self.delayField.text = [NSString stringWithFormat:@"%.1f", [AISettings replyDelay]];
+    [self.delayCard addSubview:self.delayField];
 
-    self.textView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
-    self.textView.font = [UIFont systemFontOfSize:16];
+    // 系统提示词
+    self.promptCard = makeCard();
+    [self.view addSubview:self.promptCard];
+    self.promptLabel = makeRowLabel(@"系统提示词");
+    [self.promptCard addSubview:self.promptLabel];
+    self.textView = [[UITextView alloc] initWithFrame:CGRectZero];
+    self.textView.font = [UIFont systemFontOfSize:15];
     self.textView.text = [AISettings autoSystemPrompt];
+    self.textView.backgroundColor = [UIColor clearColor];
+    self.textView.textContainerInset = UIEdgeInsetsMake(8, 4, 8, 4);
     self.textView.keyboardDismissMode = UIScrollViewKeyboardDismissModeInteractive;
-    [self.view addSubview:self.textView];
+    [self.promptCard addSubview:self.textView];
+    self.promptHint = [[UILabel alloc] initWithFrame:CGRectZero];
+    self.promptHint.text = @"自动回复时 AI 扮演你的要求";
+    self.promptHint.font = [UIFont systemFontOfSize:12];
+    self.promptHint.textColor = [UIColor secondaryLabelColor];
+    [self.promptCard addSubview:self.promptHint];
 
+    // 恢复默认
     self.resetButton = [UIButton buttonWithType:UIButtonTypeSystem];
     [self.resetButton setTitle:@"恢复默认" forState:UIControlStateNormal];
     [self.resetButton setTitleColor:[UIColor systemRedColor] forState:UIControlStateNormal];
+    self.resetButton.titleLabel.font = [UIFont systemFontOfSize:16];
+    self.resetButton.backgroundColor = [UIColor secondarySystemGroupedBackgroundColor];
+    self.resetButton.layer.cornerRadius = 12;
     [self.resetButton addTarget:self action:@selector(resetTapped) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.resetButton];
+
+    // 键盘处理：弹出时整体上移，避免挡住输入框
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
 }
 
 - (void)viewDidLayoutSubviews {
@@ -85,13 +152,66 @@
 
     CGFloat width = self.view.bounds.size.width;
     CGFloat height = self.view.bounds.size.height;
-    self.enabledLabel.frame = CGRectMake(12, 16, 120, 36);
-    self.enabledSwitch.frame = CGRectMake(width - 72, 18, 60, 30);
-    self.apiKeyField.frame = CGRectMake(12, 64, width - 24, 40);
-    self.modelField.frame = CGRectMake(12, 112, width - 24, 40);
-    self.delayField.frame = CGRectMake(12, 160, width - 24, 40);
-    self.textView.frame = CGRectMake(12, 208, width - 24, height - 208 - 90);
-    self.resetButton.frame = CGRectMake(12, height - 90, width - 24, 44);
+    CGFloat margin = 16;
+    CGFloat cardWidth = width - margin * 2;
+    CGFloat rowHeight = 48;
+    CGFloat y = margin + 4;
+
+    self.switchCard.frame = CGRectMake(margin, y, cardWidth, 56);
+    y += 56 + 12;
+
+    self.keyCard.frame = CGRectMake(margin, y, cardWidth, rowHeight);
+    y += rowHeight;
+    self.modelCard.frame = CGRectMake(margin, y, cardWidth, rowHeight);
+    y += rowHeight;
+    self.delayCard.frame = CGRectMake(margin, y, cardWidth, rowHeight);
+    y += rowHeight + 12;
+
+    CGFloat promptHeight = MAX(150, height - y - 60 - 24);
+    self.promptCard.frame = CGRectMake(margin, y, cardWidth, promptHeight);
+    y += promptHeight + 12;
+    self.resetButton.frame = CGRectMake(margin, y, cardWidth, 48);
+
+    // 卡片内部布局
+    self.enabledLabel.frame = CGRectMake(16, 0, cardWidth - 90, 56);
+    self.enabledSwitch.frame = CGRectMake(cardWidth - 60 - 14, 13, 60, 30);
+
+    [self layoutLabel:self.keyLabel field:self.apiKeyField cardWidth:cardWidth];
+    [self layoutLabel:self.modelLabel field:self.modelField cardWidth:cardWidth];
+    [self layoutLabel:self.delayLabel field:self.delayField cardWidth:cardWidth];
+
+    self.promptLabel.frame = CGRectMake(16, 12, cardWidth - 32, 20);
+    self.textView.frame = CGRectMake(12, 38, cardWidth - 24, promptHeight - 38 - 26);
+    self.promptHint.frame = CGRectMake(16, promptHeight - 22, cardWidth - 32, 16);
+}
+
+- (void)layoutLabel:(UILabel *)label field:(UITextField *)field cardWidth:(CGFloat)cardWidth {
+    label.frame = CGRectMake(16, 0, 110, 48);
+    field.frame = CGRectMake(130, 0, cardWidth - 130 - 14, 48);
+}
+
+- (void)keyboardWillShow:(NSNotification *)note {
+    CGRect kbFrame = [note.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    CGFloat covered = kbFrame.size.height - (self.view.bounds.size.height - 240);
+    if (covered > 0 && !self.keyboardVisible) {
+        self.keyboardVisible = YES;
+        [UIView animateWithDuration:0.25 animations:^{
+            self.view.transform = CGAffineTransformMakeTranslation(0, -covered);
+        }];
+    }
+}
+
+- (void)keyboardWillHide:(NSNotification *)note {
+    if (self.keyboardVisible) {
+        self.keyboardVisible = NO;
+        [UIView animateWithDuration:0.25 animations:^{
+            self.view.transform = CGAffineTransformIdentity;
+        }];
+    }
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)cancelTapped {
